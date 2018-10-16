aeab85e85575f70f292f7b25a51f8b02672570c4
MATIAS URIEL KALOMYSKY <43443777@ort>
2018-10-16 12:11:58 -0300

    16/10

---
+++ QEQ/QEQ/Models/BD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+namespace QEQ.Models
+{
+    public class BD
+    {
+        #region BD
+        private static string connectionstring = "server=10.128.8.16;Database=QEQC06;user id=QEQC06; password=QEQC06;Trusted_connection=True";
+        private static SqlConnection conectar()
+        {
+            SqlConnection a = new SqlConnection(connectionstring);
+            a.Open();
+            return a;
+        }
+        private static void desconectar(SqlConnection conexion)
+        {
+            conexion.Close();
+        }
+        #endregion
+        public static 
+    }
+}
